0d546e43de26a5ea758c66a235cc0072831948a5
Bm8725 <211398250+Bm8725@users.noreply.github.com>
2025-12-10 21:45:02 +0200

    update

--- src/app/footer/page.js
+++ src/app/footer/page.js
@@ -129,7 +129,7 @@ export default function Footer() {
           </a>
         </div>
   <p className="text-sm text-gray-600 text-center sm:text-right">
-  © {new Date().getFullYear()} UCab.ro — All rights reserved powered by <span className="font-bold text-purple-600 animate-pulse">BM</span>
+  © {new Date().getFullYear()} UCab.ro — All rights reserved.
 </p>
 
       </div>
