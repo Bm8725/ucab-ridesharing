e1954f1b3bde6d68a79bad16afa4db16cfd08ca2
Bm8725 <211398250+Bm8725@users.noreply.github.com>
2026-01-04 18:41:25 +0200

    Update+

--- src/app/lib/supabaseClient.js
+++ src/app/lib/supabaseClient.js
@@ -1,6 +1,7 @@
-//lib/supabaseClient.js
+// lib/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
+// Folosește doar cheia publică pe client
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
