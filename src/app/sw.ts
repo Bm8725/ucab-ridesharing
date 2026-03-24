bab6862911952eb56a82370ebd10554b1258bb79
smile189 <email-ul-tau-de-github@exemplu.com>
2026-03-24 23:31:18 +0200

    ucab app PWA13

--- src/app/sw.ts
+++ src/app/sw.ts
@@ -2,10 +2,13 @@
 import { defaultCache } from "@serwist/next/worker";
 import { Serwist } from "@serwist/sw"; 
 
-declare const self: ServiceWorkerGlobalScope;
+// Această parte este critică pentru ca Serwist să găsească fișierele de precache
+declare const self: ServiceWorkerGlobalScope & {
+  __SW_MANIFEST: (any | string)[] | undefined;
+};
 
 const serwist = new Serwist({
-  precacheEntries: self.__SW_MANIFEST,
+  precacheEntries: self.__SW_MANIFEST, // Next.js va injecta aici lista de fișiere
   skipWaiting: true,
   clientsClaim: true,
   navigationPreload: true,
