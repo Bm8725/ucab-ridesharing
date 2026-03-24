4434c828814ff407610d0650a8d557f5eade8741
smile189 <email-ul-tau-de-github@exemplu.com>
2026-03-24 22:30:26 +0200

    ucab app PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
+import withSerwistInit from "@serwist/next";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
 };
 
-export default nextConfig;
+const withSerwist = withSerwistInit({
+  swSrc: "app/sw.ts",         // Sursa codului de fundal (Service Worker)
+  swDest: "public/sw.js",     // Fișierul generat automat
+  disable: process.env.NODE_ENV === "development", // Dezactivează în dev ca să nu te încurce la teste
+});
+
+export default withSerwist(nextConfig);
