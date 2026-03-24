a846afbf720dcf359f28a4a5511d3157d32ad841
smile189 <email-ul-tau-de-github@exemplu.com>
2026-03-24 22:59:02 +0200

    ucab app PWA1

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-// ADAUGĂ "default" AICI:
+// Atenție: export DEFAULT function PROXY
 export default function proxy(request: NextRequest) { 
   const { pathname } = request.nextUrl;
 
