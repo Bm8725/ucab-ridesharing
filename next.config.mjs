77077c4c64617a3b4d6f4ede20de9f5b516132df
smile189 <email-ul-tau-de-github@exemplu.com>
2026-03-24 23:10:49 +0200

    ucab app PWA13

--- next.config.mjs
+++ next.config.mjs
@@ -3,18 +3,6 @@ import withSerwistInit from "@serwist/next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
-  // Această linie forțează dezactivarea Turbopack în Next.js 16
-  turbopack: {}, 
-  // Forțăm configurarea Webpack pentru Serwist
-  webpack: (config) => {
-    return config;
-  },
 };
 
-const withSerwist = withSerwistInit({
-  swSrc: "app/sw.ts",
-  swDest: "public/sw.js",
-  disable: process.env.NODE_ENV === "development",
-});
-
-export default withSerwist(nextConfig);
+export default nextConfig;
