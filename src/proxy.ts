9daf23915e6cfee36642bb5e6e269a30d9261b8c
smile189 <email-ul-tau-de-github@exemplu.com>
2026-03-24 22:54:53 +0200

    ucab app PWA1

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,19 +1,17 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function proxy(request: NextRequest) {
+// Schimbă "middleware" în "proxy" aici:
+export function proxy(request: NextRequest) { 
   const { pathname } = request.nextUrl;
 
   if (pathname === '/') {
-    const isLogged = false; // Logica ta de auth (ex: verificare cookie)
+    const isLogged = false; 
 
     if (isLogged) {
-      // Dacă e logat, îl trimiți la dashboard
       return NextResponse.redirect(new URL('/dashboard', request.url));
     }
     
-    // Dacă NU e logat, îl lași pe "/" (Landing Page)
-    // NU folosi redirect către "/" aici, pentru că ești deja pe "/"
     return NextResponse.next();
   }
 }
